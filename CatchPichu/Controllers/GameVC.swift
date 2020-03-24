5053c2b9f0f031d421d3b9adf9fdfb1ddaa74c66
Penny Huang <penny.huang.pw@outlook.com>
2020-03-24 22:49:19 +0800

    add UserDefault

--- CatchPichu/Controllers/GameVC.swift
+++ CatchPichu/Controllers/GameVC.swift
@@ -22,9 +22,17 @@ class GameVC: UIViewController {
     let pikachuArray = Array(repeating: "pikachu", count: 2)
     let raichuArray = Array(repeating: "raichu", count: 1)
     var gameTime = 10
+    
+    var pichuNum = 0
+    var pikachuNum = 0
+    var raichuNum = 0
     var totalScroe = 0
     
     let player = AVPlayer()
+    
+    let defaults = UserDefaults.standard
+    var initialScoreArray = [0, 0, 0, 0, 0,]
+
     
     
     @IBAction func StartButtonPressed(_ sender: UIButton) {
@@ -44,10 +52,13 @@ class GameVC: UIViewController {
             switch pokemonHit.titleLabel?.text {
             case "pikachu":
                 totalScroe += 3
+                pikachuNum += 1
             case "raichu":
                 totalScroe += 5
+                raichuNum += 1
             default:
                 totalScroe += 1
+                pichuNum += 1
             }
             
             scoreLabel.text = "\(totalScroe)"
@@ -75,7 +86,7 @@ class GameVC: UIViewController {
     func gameStart() {
         
         // Timer: game countdown
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+        Timer.scheduledTimer(withTimeInterval: Double.random(in: 0.7...1.5), repeats: true) { (timer) in
             self.gameTime -= 1
             self.gameTimeLabel.text = String(self.gameTime)
             if self.gameTime == 0 {
@@ -87,7 +98,7 @@ class GameVC: UIViewController {
         }
         
         // Timer: showing pokemon until time's up
-        Timer.scheduledTimer(withTimeInterval: 1.5, repeats: true) { (timer) in
+        Timer.scheduledTimer(withTimeInterval: 1.2, repeats: true) { (timer) in
             self.showPokemon()
             if self.gameTime == 0 {
                 timer.invalidate()
@@ -137,14 +148,14 @@ class GameVC: UIViewController {
 //                print(emptyButtonArray[i].titleLabel?.text ?? "cannot get button title")
 
                 //每種pokemon出現的時間
-                var pokemonAppearTime = 3
+                var pokemonAppearTime: Float = 3.0
                 switch randomPokemon {
                 case "pikachu":
-                    pokemonAppearTime = 2
+                    pokemonAppearTime = Float.random(in: 1.5 ... 2.5)
                 case "raichu":
                     pokemonAppearTime = 1
                 default:
-                    pokemonAppearTime = 3
+                    pokemonAppearTime = Float.random(in: 2...4)
                 }
                 
                 //只讓該次出現的pokemon消失
@@ -166,11 +177,57 @@ class GameVC: UIViewController {
     func showTimesUpAlert(){
         let alert = UIAlertController(title: "TIME'S UP!", message: "Your score is  \(totalScroe)", preferredStyle: .alert)
         let seeResultAction = UIAlertAction(title: "See Result", style: .default) { (alertAction) in
+            
+            self.storeLatestResult()
+            self.storeRank()
             self.performSegue(withIdentifier: "gameToLeaderboardSG", sender: self)
         }
         
         alert.addAction(seeResultAction)
         present(alert, animated: true, completion: nil )
+        
+    }
+    
+    func storeLatestResult(){
+        let resultArray = [pichuNum, pikachuNum, raichuNum, totalScroe]
+        defaults.set(resultArray, forKey: "ResultArray")
+    }
+    
+    func storeRank() {
+        
+        var temp: Int!
+        var comparedScore = totalScroe
+        
+        // Check if the app is played for the first time on the device
+        if defaults.array(forKey: "ScoreArray") != nil {
+            
+            var scoreArray =  defaults.array(forKey: "ScoreArray")as! [Int]
+            
+            for i in 0 ..< 5 {
+                
+                if comparedScore >= scoreArray[i] {
+                    temp = scoreArray[i]
+                    scoreArray[i] = comparedScore
+                    comparedScore = temp
+                }
+            }
+            defaults.set(scoreArray, forKey: "ScoreArray")
+        
+        } else {
+            // Build a UserDefault if user plays for the first time
+            for i in 0 ..< 5 {
+                
+                if comparedScore >= initialScoreArray[i] {
+                    temp = initialScoreArray[i]
+                    initialScoreArray[i] = comparedScore
+                    comparedScore = temp
+                }
+            }
+            defaults.set(initialScoreArray, forKey: "ScoreArray")
+            
+        }
+        
+
         
     }
     
--- CatchPichu/Controllers/LeaderboardVC.swift
+++ CatchPichu/Controllers/LeaderboardVC.swift
@@ -9,15 +9,48 @@
 import UIKit
 
 class LeaderboardVC: UIViewController {
-
+    
+    let defaults = UserDefaults.standard
+    
+    
+    @IBOutlet var lastResultLabel: [UILabel]!
+    
+    
+    @IBOutlet var rankLabel: [UILabel]!
     
     @IBAction func returnButtonPressed(_ sender: Any) {
 
     }
     
     
+    func displayLastResult(){
+        let latestResultArray = defaults.array(forKey: "ResultArray") as! [Int]
+        
+        lastResultLabel[0].text = "PICHU: \(latestResultArray[0])"
+        lastResultLabel[1].text = "PIKACHU: \(latestResultArray[1])"
+        lastResultLabel[2].text = "RAICHU: \(latestResultArray[2])"
+        lastResultLabel[3].text = "SCORE: \(latestResultArray[3])"
+        
+    }
+    
+    func displayRank(){
+        let scoreArray = defaults.array(forKey: "ScoreArray") as! [Int]
+        rankLabel[0].text = "1st \(scoreArray[0])"
+        rankLabel[1].text = "1st \(scoreArray[1])"
+        rankLabel[2].text = "1st \(scoreArray[2])"
+        rankLabel[3].text = "1st \(scoreArray[3])"
+        rankLabel[4].text = "1st \(scoreArray[4])"
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        displayLastResult()
+        displayRank()
+        
+//        print(latestResultArray ?? "cannot find the latest result in UserDefault")
+//        print(scoreArray ?? "cannot find score array in UserDefault")
 
     }
     
