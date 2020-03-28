6398917dc5fe21715e3479ac22df675c1312fe8c
Penny Huang <penny.huang.pw@outlook.com>
2020-03-28 15:32:15 +0800

    fix unwrap nil bug happening when open the app for the first time. fix the label display

--- CatchPichu/Controllers/LeaderboardVC.swift
+++ CatchPichu/Controllers/LeaderboardVC.swift
@@ -24,22 +24,40 @@ class LeaderboardVC: UIViewController {
     
     
     func displayLastResult(){
-        let latestResultArray = defaults.array(forKey: "ResultArray") as! [Int]
         
-        lastResultLabel[0].text = "PICHU: \(latestResultArray[0])"
-        lastResultLabel[1].text = "PIKACHU: \(latestResultArray[1])"
-        lastResultLabel[2].text = "RAICHU: \(latestResultArray[2])"
-        lastResultLabel[3].text = "SCORE: \(latestResultArray[3])"
+        // Check if there are previous game results
+        if defaults.array(forKey: "ResultArray") != nil {
+            let latestResultArray = defaults.array(forKey: "ResultArray") as! [Int]
+            lastResultLabel[0].text = "PICHU: \(latestResultArray[0])"
+            lastResultLabel[1].text = "PIKACHU: \(latestResultArray[1])"
+            lastResultLabel[2].text = "RAICHU: \(latestResultArray[2])"
+            lastResultLabel[3].text = "SCORE: \(latestResultArray[3])"
+        } else {
+            lastResultLabel[0].text = "PICHU: 0"
+            lastResultLabel[1].text = "PIKACHU: 0"
+            lastResultLabel[2].text = "RAICHU: 0"
+            lastResultLabel[3].text = "SCORE: 0"
+        }
+        
         
     }
     
     func displayRank(){
-        let scoreArray = defaults.array(forKey: "ScoreArray") as! [Int]
-        rankLabel[0].text = "1st \(scoreArray[0])"
-        rankLabel[1].text = "2st \(scoreArray[1])"
-        rankLabel[2].text = "3st \(scoreArray[2])"
-        rankLabel[3].text = "4st \(scoreArray[3])"
-        rankLabel[4].text = "5st \(scoreArray[4])"
+        if defaults.array(forKey: "ScoreArray") != nil {
+            let scoreArray = defaults.array(forKey: "ScoreArray") as! [Int]
+            rankLabel[0].text = "1st \(scoreArray[0])"
+            rankLabel[1].text = "2nd \(scoreArray[1])"
+            rankLabel[2].text = "3rd \(scoreArray[2])"
+            rankLabel[3].text = "4th \(scoreArray[3])"
+            rankLabel[4].text = "5th \(scoreArray[4])"
+        } else {
+            rankLabel[0].text = "1st 0"
+            rankLabel[1].text = "2nd 0"
+            rankLabel[2].text = "3rd 0"
+            rankLabel[3].text = "4th 0"
+            rankLabel[4].text = "5th 0"
+        }
+        
         
     }
     
